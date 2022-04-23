cf58dfe4e4f06542187711855fc9fc9d8f3a176d
Frederic Ellsworth <61885939+SpartanFrederic104@users.noreply.github.com>
2022-04-22 20:55:39 -0700

    Add BUTTERFLY Note arrow

---
+++ dance/Butterfly_Note/Left Hold Head Inactive.lua
@@ -0,0 +1,8 @@
+return Def.Sprite {
+	Texture=NOTESKIN:GetPath('_left','hold inactive');
+	Frames = Sprite.LinearFrames( 1, 1 );
+	InitCommand=cmd(effectclock,'beat';);
+	DrawTapNoteMessageCommand=function(self,parent)
+		parent:spin();
+	end;
+};
